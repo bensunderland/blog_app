3c63d543e1862243dc3437a76b6d75a2dbc07d84
Benjamin Sunderland <bensunderland@proton.me>
2024-10-15 17:54:32 +0100

    update added

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   
   root 'blog_posts#index'
 
+  get 'blog_posts/new', to: 'blog_posts#new', as: :new_blog_post
+  get 'blog_posts/:id/edit', to: 'blog_posts#edit', as: :edit_blog_postpat
+  patch "blog_posts/:id", to: "blog_posts#update"
   get 'blog_posts/:id', to: 'blog_posts#show', as: :blog_post
+  post '/blog_posts', to: "blog_posts#create", as: :blog_posts
   # Defines the root path route ("/")
   # root "posts#index"
 end
