9ec7d07ddc2b7dce7717d23823b5c66dd1f7eab2
Benjamin Sunderland <bensunderland@proton.me>
2024-10-15 16:06:58 +0100

    added show

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   
   root 'blog_posts#index'
 
-  get 'blog_posts/:id', to: 'blog_posts#show'
+  get 'blog_posts/:id', to: 'blog_posts#show', as: :blog_post
   # Defines the root path route ("/")
   # root "posts#index"
 end
