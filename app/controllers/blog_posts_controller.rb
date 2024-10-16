8d67b45541cb285cc5721de70059038732ecca17
Benjamin Sunderland <bensunderland@proton.me>
2024-10-16 13:36:06 +0100

    added devise and tailwind

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,12 @@
 class BlogPostsController < ApplicationController
+
+    before_action :set_blog_post, except: [:index, :new, :create]
+    before_action :authenticate_user!, except: [:index, :show]
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def new 
@@ -12,7 +14,6 @@ class BlogPostsController < ApplicationController
     end
 
     def edit
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def create
@@ -26,20 +27,26 @@ class BlogPostsController < ApplicationController
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
+       
         if @blog_post.update(blog_post_params)
             redirect_to @blog_post 
         else 
             render :edit, status: :unprocessable_enetityd
         end
-        
-        
     end
 
+    def destroy 
+        @blog_post.destroy
+        redirect_to root_path
+    end
 
     private 
     
     def blog_post_params
         params.require(:blog_post).permit(:title, :body)
+    end
+
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
     end
 end
